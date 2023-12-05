c8289a0b7e225dd1c089f9fb53fdcdba77c5aedc
BableshAAzad <bableshaazad@outlook.com>
2023-12-05 23:12:38 +0530

    css class

---
+++ BackgroundProperties/BGproperties.js
@@ -0,0 +1,9 @@
+let obj = {
+     name : "bablesh",
+     sno : 123,
+     email: "aazadbablesh@gmail.com",
+}
+console.log(obj);
+obj.sno = 111;
+obj.add = "Raipur";
+console.log(obj);
---
+++ TextProperty/text.js
@@ -0,0 +1,5 @@
+let obj = {
+    name:"bablesh",
+    mono: 7898300815,
+}
+// console.log(ObjectId(obj));
